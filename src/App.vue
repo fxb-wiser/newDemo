940946097173234fe21cc6ba3f07a84740b9e661
fxb-wiser <57519034+fxb-wiser@users.noreply.github.com>
2023-11-29 10:42:20 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  头像：<img src=".assets/logo.png">
+  <p>学号：2310275026  昵称：champagne</p>
 </template>
 
 <script>
